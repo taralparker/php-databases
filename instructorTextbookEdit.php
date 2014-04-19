56dc1d44fc51ea46daa79d4cf9c04a38b6320c25
balbot <b.albot@ttu.edu>
2014-04-19 14:08:10 -0500

    Update instructorTextbookEdit.php
    Added comments

--- instructorTextbookEdit.php
+++ instructorTextbookEdit.php
@@ -1,3 +1,4 @@
+<!--Copied from templateTable.php-->
 <?php
 	// Change this to "faculty", "instructor", or "business"
 	$pageSessionType = "instructor";
@@ -5,10 +6,11 @@
 	// Include this for global database access variables
 	include "databaseSettings.php";
 ?>
-
+<!-- This is actual html-->
 <html>
  <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <!-- Set page title here -->
   <title>Textbook Editor</title>
   <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
  </head>
@@ -26,35 +28,24 @@
 
 	function convertTime($time)
 	{
+		//This function converts a string from military time to HH:MM format
 		$timeString = ((floor($time/100) % 12 == 0) ? "12" : (floor($time/100) % 12)) . ":" . (($time % 100 == 0) ? "00" : ($time % 100));
 		return $timeString;
 	}
 	
+	//Creates a new MySQL object to connect to the local database
  	$mysqli = new mysqli( $dbproHost , $dbproUsername , $dbproPassword , $dbproSchema );
 
 	if( !$mysqli->connect_errno )
 	{
+		//For now, year and semester either default to Spring 2014 or can be changed in instructorHome.php and kept track of by the session
 		$year = isset( $_SESSION[ "demoYear" ] ) ? $_SESSION[ "demoYear" ] : 2014;
-		$semester = isset( $_SESSION[ "demoSemester" ] ) ? $_SESSION[ "demoSemester" ] : "Spring"; //@TODO: to change this
-		// If the page was submitted with an update request, do so
-		/*if( !empty( $_POST ) )
-		{
-			if( !empty( $_POST[ "rNumber" ] ) && !empty( $_POST[ "lastName" ] ) && !empty( $_POST[ "firstName" ] ) && !empty( $_POST[ "tenured" ] ) && !empty( $_POST[ "joiningSemester" ] ) && !empty( $_POST[ "joiningYear" ] ) )
-			{
-				$tenu = $_POST[ "tenured" ] === "Y" ? 1 : 0;
-				$sql = "INSERT INTO instructors( rNumber , lastName , firstName , instructorTitle , tenured , joiningSemester ,
-joiningYear , loadPreference ) VALUES ('$_POST[rNumber]','$_POST[lastName]','$_POST[firstName]','$_POST[instructorTitle]',$tenu,'$_POST[joiningSemester]',$_POST[joiningYear], NULL )";
-				//printf( "%s<br>" , $sql );
-
-				if( $mysqli->query( $sql ) )
-					printf( "Insertion complete.<br>" );
-				else
-					printf( "Insertion failed: %s<br>" , $mysqli->error );
-			}
-			else
-				printf( "One or more required fields are blank." );
-		}*/
-
+		$semester = isset( $_SESSION[ "demoSemester" ] ) ? $_SESSION[ "demoSemester" ] : "Spring";
+		//@TODO: get the semester/year dynamically instead of hardcoding it
+		
+		//The SQL query, whitespace doesn't matter (except for spaces between words ofc)
+		//year=$year because it's an integer, semester='$semester' because it's a string
+		//rNumber is stored by the session, so that's how it's accessed
 		$sql = "
 		select * from
 		((select * from taughtBy natural join consistsOf natural join Courses natural join Sections
@@ -65,9 +56,12 @@ joiningYear , loadPreference ) VALUES ('$_POST[rNumber]','$_POST[lastName]','$_P
 		(select CRN, year, semester from taughtBy where rNumber=$_SESSION[rNumber])) as C)
 		order by courseCode, CRN
 		";
+		//Can also run queries like this, from inline strings
 		if( $result = $mysqli->query( "select lastName, firstName from instructors where rNumber = $_SESSION[rNumber];" ) )
 		{
+			//This puts the first (technically the next remaining) row into the $row variable, whose columns can be accessed as follows
 			$row = $result->fetch_array( MYSQLI_ASSOC );
+			//echo prints out html code, . (period) is the php string concatenation operator, and $row['attributeName'] returns the value of that attribute in this row
 			echo "<p>Textbook information for " . $row [ "firstName" ] . " " . $row [ "lastName" ] . " (" . $_SESSION['rNumber'] . ") in " . $semester . " " . $year . ".</p>";
 		}
 		else
@@ -76,8 +70,11 @@ joiningYear , loadPreference ) VALUES ('$_POST[rNumber]','$_POST[lastName]','$_P
 			echo "<div align='center'>Invalid request. Please contact a system administrator.</div>";
 		}
 		// Start making the table
+		//Here we use the complicated query from above
 		if( $result = $mysqli->query( $sql ))
 		{
+		//Just create the header row with titles
+		//Need this id and class on the table for css and javascript purposes
 			echo "<table border='1' id='htmlgrid' class='testgrid'>
 			<tr>
 			<th>Course Code</th>
@@ -95,8 +92,11 @@ joiningYear , loadPreference ) VALUES ('$_POST[rNumber]','$_POST[lastName]','$_P
 			</tr>";
 			
 			// Create each row
+			//Iterate through the rows returned
 			while( $row = $result->fetch_array( MYSQLI_ASSOC ) )
 			{
+				//Print out rows to match the header, the attributeNames are just what they would be in SQL
+				//Times are converted to look good
 				echo "<tr>";
 				echo "<td>" . $row[ "courseCode" ] . "</td>";
 				echo "<td>" . $row[ "courseTitle" ] . "</td>";
@@ -113,8 +113,8 @@ joiningYear , loadPreference ) VALUES ('$_POST[rNumber]','$_POST[lastName]','$_P
 				echo "</tr>";
 			}
 
-			echo "</table>";
-
+			echo "</table>"; //Close the table
+			//Everything past here is error stuff and boilerplate
 			// Clean up
 			$result->close();
 		}
